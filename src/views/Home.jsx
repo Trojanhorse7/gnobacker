3a41e31dd06e05e3ae6d2a814332b0b5b4bcf4e0
Trojan <felixgogodae777@gmail.com>
2023-05-25 15:08:28 +0100

    Updated Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react'
-import AddButton from '../components/AddButton'
-import CreateProject from '../components/CreateProject'
-import Hero from '../components/Hero'
-import Projects from '../components/Projects'
-import { loadProjects } from '../services/blockchain'
-import { useGlobalState } from '../store'
+import { useEffect, useState } from "react";
+import AddButton from "../components/AddButton";
+import CreateProject from "../components/CreateProject";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import { useWeb3 } from "../services/useWeb3";
+import { useGlobalState, setGlobalState } from "../store";
+import {fetchSigner} from "@wagmi/core";
 
 const Home = () => {
-  const [projects] = useGlobalState('projects')
+	const { loadProjects } = useWeb3();
 
-  
+	useEffect(async () => {
+		await loadProjects();
+	}, []);
 
-  useEffect(async () => {
-    await loadProjects()
-  }, [])
-  return (
-    <>
-      <Hero />
-      <Projects projects={projects} />
-      <CreateProject />
-      <AddButton />
-    </>
-  )
-}
+	return (
+		<>
+			<Hero />
+			<Projects />
+			<CreateProject />
+			<AddButton />
+		</>
+	);
+};
 
-export default Home
+export default Home;
