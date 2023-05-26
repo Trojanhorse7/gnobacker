b118ecdda0f1a16a400dcfb011076189aaa67470
Trojan <felixgogodae777@gmail.com>
2023-05-26 14:27:10 +0100

    Updated Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,40 +18,40 @@ const Projects = () => {
   }, [projects, end])
 
   return (
-    <div className="flex flex-col px-9 mb-7 width-[100%] bg-green-700">
-      <div className="flex justify-center items-center flex-wrap">
-        {collection.map((project, i) => (
-          <ProjectCard key={i} project={project} />
-        ))}
-      </div>
+		<div className="flex flex-col px-9 mb-7 width-[100%] bg-green-700 border-y-4 border-dotted border-green-200">
+			<div className="flex justify-center items-center flex-wrap">
+				{collection.map((project, i) => (
+					<ProjectCard key={i} project={project} />
+				))}
+			</div>
 
-      {projects.length > collection.length ? (
-        <div className="flex justify-center items-center my-5">
-          <button
-            type="button"
-            className="inline-block px-6 py-2.5 bg-green-600
+			{projects.length > collection.length ? (
+				<div className="flex justify-center items-center my-5">
+					<button
+						type="button"
+						className="inline-block px-6 py-2.5 bg-green-600
           text-white font-medium text-xs leading-tight uppercase
           rounded-full shadow-md hover:bg-green-700"
-            onClick={() => setEnd(end + count)}
-          >
-            Load more
-          </button>
-        </div>
-      ) : null}
-    </div>
-  )
+						onClick={() => setEnd(end + count)}
+					>
+						Load more
+					</button>
+				</div>
+			) : null}
+		</div>
+	);
 }
 
 const ProjectCard = ({ project }) => {
   const expired = new Date().getTime() > Number(project?.expiresAt + '000')
 
   return (
-    <div id="projects" className="rounded-lg shadow-lg bg-white w-64 m-4">
+    <div id="projects" className="rounded-sm shadow-lg bg-green-200 w-[20rem] m-4">
       <Link to={'/projects/' + project.id}>
         <img
           src={project.imageURL}
           alt={project.title}
-          className="rounded-xl h-64 w-full object-cover"
+          className="rounded-xl h-[20rem] w-full object-fit"
         />
 
         <div className="p-4">
